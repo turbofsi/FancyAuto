ae043123be062fb366c3c4ed1169a9f18cc59ab6
yang wang <yangwangbit@gmail.com>
2017-09-13 22:34:16 -0400

    setup product list component

--- APM/src/app/app.component.ts
+++ APM/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'fc-root',
+  template:`
+    <div>
+      <h1>{{pageTitle}}</h1>
+      <fc-products></fc-products>
+     </div>
+  `
 })
+
 export class AppComponent {
-  title = 'Angular: Getting Started. Awesome YangTech';
+  pageTitle: string = 'Fancy Cars'
 }
